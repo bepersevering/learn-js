8c9c71d4f1614373f3abbc3c1c4c9e98a092f565
aikongfu <aikongfu@163.com>
2022-03-29 18:23:23 +0800

    fix playFor method parameter

--- memo-backend/service/statement/statement.v2.js
+++ memo-backend/service/statement/statement.v2.js
@@ -10,6 +10,39 @@
  * @return {string} result - 结算单
  */
 function statement(invoice, plays) {
+  /**
+   * calculate performance for play
+   * @param {Object} aPerformance - a performance
+   * @param {string} aPerformance.playID - invoice performances playID
+   * @param {string} aPerformance.audience - invoice performances audience
+   * @param {Object} play - play
+   * @param {string} play.name - play name
+   * @param {string} play.type - play type
+   * @return {Number} amount
+   */
+  function amountFor(aPerformance, play) {
+    let amount = 0;
+    switch (play.type) {
+    case 'tragedy':
+      amount = 40000;
+      if (aPerformance.audience > 30) {
+        amount = amount + 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      amount = 30000;
+      if (aPerformance.audience > 20) {
+        amount = amount + (10000 + 500 * (aPerformance.audience - 20));
+      }
+      amount = amount + 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return amount;
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -24,7 +57,7 @@ function statement(invoice, plays) {
   });
 
   for (const perf of invoice.performances) {
-    const play = playFor(plays[perf.playID]);
+    const play = playFor(perf);
     const thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -38,39 +71,6 @@ function statement(invoice, plays) {
   result = `${result}Amount owed is ${format(totalAmount / 100)}\n`;
   result = `${result}You earned ${volumeCredits} credits\n`;
   return result;
-}
-
-/**
- * calculate performance for play
- * @param {Object} aPerformance - a performance
- * @param {string} aPerformance.playID - invoice performances playID
- * @param {string} aPerformance.audience - invoice performances audience
- * @param {Object} play - play
- * @param {string} play.name - play name
- * @param {string} play.type - play type
- * @return {Number} amount
- */
-function amountFor(aPerformance, play) {
-  let amount = 0;
-  switch (play.type) {
-  case 'tragedy':
-    amount = 40000;
-    if (aPerformance.audience > 30) {
-      amount = amount + 1000 * (aPerformance.audience - 30);
-    }
-    break;
-  case 'comedy':
-    amount = 30000;
-    if (aPerformance.audience > 20) {
-      amount = amount + (10000 + 500 * (aPerformance.audience - 20));
-    }
-    amount = amount + 300 * aPerformance.audience;
-    break;
-  default:
-    throw new Error(`unknown type: ${play.type}`);
-  }
-
-  return amount;
 }
 
 module.exports = {
